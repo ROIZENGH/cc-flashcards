471bd74ac5c3da8e1c23903f2c2c7f51a587faf3
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-04-22 18:16:57 -0500

    create slice with addTopic reducer

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    topics: {
+        
+    }
+}
+
+const topicsSlice = createSlice({
+    name: "topics",
+    initialState: initialState,
+    reducers: {
+        addTopic: (state, action) => {
+            const newTopic = {...action.payload, quizIds:[]}
+            state.topics[action.payload.id].push(newTopic);
+        }
+    },
+    extraReducers: {
+
+    }
+});
+
+export const selectTopics = (state) => state.topics;
+
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
