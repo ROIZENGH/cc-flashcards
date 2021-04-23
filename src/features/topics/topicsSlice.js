c1b9e50335a3a2757ff1fa0ae1d3e2c07a2df1d6
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-04-23 10:26:21 -0500

    add and export addQuizId action

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -13,6 +13,9 @@ const topicsSlice = createSlice({
         addTopic: (state, action) => {
             const newTopic = {...action.payload, quizIds:[]};
             state.topics[action.payload.id] = newTopic;
+        },
+        addQuizId: (state, action) => {
+            state.topics[action.payload.topicId].quizIds.push(action.payload.quizId);
         }
     },
     extraReducers: {
@@ -22,5 +25,5 @@ const topicsSlice = createSlice({
 
 export const selectTopics = (state) => state.topics.topics;
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
