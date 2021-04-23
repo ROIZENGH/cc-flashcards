d3782aa8b4d09dd8069f1a0bf1255d05de3e6974
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-04-23 12:24:55 -0500

    readability

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -19,6 +19,7 @@ export default function NewTopicForm() {
     if (name.length === 0) {
       return;
     }
+    // dispatch your add topic action here
     dispatch(
       addTopic({
         name: name,
@@ -26,7 +27,6 @@ export default function NewTopicForm() {
         id: uuidv4()
       })
     );
-    // dispatch your add topic action here
     history.push(ROUTES.topicsRoute());
   };
 
